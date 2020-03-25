2b0f8a52c48c37708a7e8a4e6fbceddfeb7315c7
Nimit <nimitkhuranaa@gmail.com>
2020-03-25 11:56:36 +0530

    improcements

---
+++ imageViewer.js
@@ -0,0 +1,12 @@
+import Viewer from 'viewerjs';
+
+const viewer = new Viewer(document.getElementById('images'), {
+    inline: true,
+    viewed() {
+        viewer.zoomTo(1);
+    },
+});
+// Then, show the image by click it, or call `viewer.show()`.
+
+// View a list of images
+//const gallery = new Viewer(document.getElementByClassName('images'));
